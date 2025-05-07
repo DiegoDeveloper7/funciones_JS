8f62a98daeca975ea2038d618ac7b7b7b943b90c
DiegoDeveloper7 <diegoag8@gmail.com>
2025-05-07 17:39:53 -0400

    2. creacion de arrow function

---
+++ assets/js/2_arrow.js
@@ -0,0 +1,11 @@
+// Transforma la siguiente función a una arrow function de una línea.
+
+let suma = function(a, b){
+    return a + b
+    }
+
+// Arrow function 
+
+let sumaD = (a,b) => a+b;
+
+console.log(sumaD(7,8))
