d25c12643c07a4f067a0e75f8aeef3195f1c052c
DiegoDeveloper7 <diegoag8@gmail.com>
2025-05-08 11:59:33 -0400

    6. creacion funcion pintarM2 que modifica el color con un parametro 'color' y activa la funcion por defecto en color verde

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,7 +9,9 @@
 Modifica la función para que reciba el elemento clickeado de forma de no
 tener que seleccionarlo nuevamente dentro de la función */
 
-// Funcion modificada (M1), para recibir elemento clickeado.
+/* 
+
+Funcion modificada (M1), para recibir elemento clickeado.
 
 function pintarM1(event) {
     event.target.style.backgroundColor = 'yellow';
@@ -17,6 +19,23 @@ function pintarM1(event) {
 
   const ele = document.getElementById("ele1");
   ele.addEventListener("click", pintarM1);
+  
+  */
+
+
+
+  function pintarM2(elemento, color='#39FF14') {
+    elemento.style.backgroundColor = color;
+  }
+
+  const ele = document.getElementById("ele1");
+
+  // Al hacer clic, se ejecuta la función y se pasa 'yellow'
+  ele.addEventListener("click", (event)=> {
+    pintarM2(event.target, 'yellow');
+  });
+
+  pintarM2(ele); // Aplica 'green' de inmediato
 
 
 
