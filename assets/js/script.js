4caa8f580dc86c7a3b7414cf2b5bbd9ffc11abe7
DiegoDeveloper7 <diegoag8@gmail.com>
2025-05-08 11:17:44 -0400

    5. creacion funcion pintarM para capturar el evento click sin llamar el id del div

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,22 @@
 
-function pintar(){
+/* function pintar(){
     ele.style.backgroundColor = 'yellow'
     }
     const ele = document.getElementById("ele1")
     ele.addEventListener("click", pintar);
+
+
+Modifica la función para que reciba el elemento clickeado de forma de no
+tener que seleccionarlo nuevamente dentro de la función */
+
+// Funcion modificada (M1), para recibir elemento clickeado.
+
+function pintarM1(event) {
+    event.target.style.backgroundColor = 'yellow';
+  }
+
+  const ele = document.getElementById("ele1");
+  ele.addEventListener("click", pintarM1);
+
+
+
